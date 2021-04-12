bb85c5bc0a4c22b2b60ef9960feda8d0f710e9e7
jcgs98 <80323289+jcgs98@users.noreply.github.com>
2021-04-12 20:14:23 -0300

    Update Calc_GET.php

--- Calc_GET.php
+++ Calc_GET.php
@@ -7,23 +7,22 @@
 	if(!is_numeric($x) || !is_numeric($y) || empty($op))
 	{
 		echo "<font size='5'><strong>Erro! Existem campos vazios.</strong></font>";		
-    }
+    	}
 
 	else if ((!$y)&&($op =="/")||(!$y)&&($op =="%"))
 	{
 		echo "<font size='5'><strong>Não é possível realizar uma divisão por zero</strong></font>";
-    }
+    	}
 
 	else
 	{
-		$resultado 	=	(	$op	==	"+"	)	? (	$x 	+ 	$y )  	:
-						((	$op	==	"-"	)	?	$x 	- 	$y	    :
-						((	$op	==	"*"	)	?	$x 	* 	$y	    :
-						((	$op	==	"%"	)	?	$x 	% 	$y	    :
-						((	$op	==	"**")	?	$x 	**	$y		:
-												$x 	/ 	$y))))	;
+		$resultado 	=	(	$op	==	"+"	)	? (	$x 	+ 	$y	)	:
+					((	$op	==	"-"	)	?	$x 	- 	$y	    	:
+					((	$op	==	"*"	)	?	$x 	* 	$y	    	:
+					((	$op	==	"%"	)	?	$x 	% 	$y	    	:
+					((	$op	==	"**"	)	?	$x 	**	$y		:
+											$x 	/ 	$y	))))	;
 
 		echo "<font size='5'><strong>Resultado de $x $op $y = $resultado.</strong></font>";
 	}
-
 ?>
