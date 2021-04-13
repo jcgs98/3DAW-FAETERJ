4dfd861b273bb8e10cb579f95772c687f1036c3a
jcgs98 <80323289+jcgs98@users.noreply.github.com>
2021-04-13 20:10:29 -0300

    Add files via upload
    insistindo com eval

--- Calc_POST.php
+++ Calc_POST.php
@@ -2,22 +2,42 @@
 	include("Calc_POST.html");
 	$x 	= $_POST['x'];
 	$y 	= $_POST['y'];
-	$op	= $_POST['operacao'];
+	$op = $_POST['operacao'];
+	
+	function X_mais_Y(int $x, int $y) {
+		return $x+$y;
+	}
+	function X_menos_Y(int $x, int $y) {
+		return $x-$y;
+	}
+	function X_porcento_de_Y(int $x, int $y) {
+		return ($x/100)*$y;
+	}
+	function raiz_Y_de_X(int $x, int $y) {
+		return $x**(1/$y);
+	}
+	function X_vezes_Y(int $x, int $y) {
+		return $x*$y;
+	}
+	function X_dividido_por_Y(int $x, int $y) {
+		return $x/$y;
+	}
+	function inverso_de_X(int $x, int $y) {
+		return 1/$x;
+	}
+	function X_elevado_a_Y(int $x, int $y) {
+		return $x**$y;
+	}
 
 	if(!is_numeric($x) || !is_numeric($y) || empty($op))
 	{
 		echo "<font size='5'><strong>Erro! Existem campos vazios.</strong></font> ";
-    	}
+    }
 	
-	else if ((!$y)&&($op =="/")||(!$y)&&($op =="%"))
+	else if ((!$y)&&($op=="X / Y"))
 	{
-        echo "<font size='5'><strong>Erro! Não é possível realizar uma divisão por zero.</strong></font> ";
+		echo "<font size='5'><strong>Não é possível realizar uma divisão por zero</strong></font>";
 	}
 	
-	else
-	{
-		$resultado="$x $op $y";
-		echo "$x $op $y = ";
-		eval ('echo '.$resultado.';');
-	}
+	else eval ('echo '.$op($x,$y).';');
 ?>
